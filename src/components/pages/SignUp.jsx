2ee09f070f776d676a50abc13a1031813695d11c
SaulWeaponz <saulweaponz@gmail.com>
2025-05-28 12:01:43 +0300

    Add SignUp form

--- src/components/pages/SignUp.jsx
+++ src/components/pages/SignUp.jsx
@@ -1,13 +1,57 @@
 import Footer from "../sections/Footer";
 
-function SignUp(){
-    return(
-        <div>
-            <h1>SignUp Page</h1>
-            <div>
-                <Footer/>
+function SignUp() {
+  return (
+    <div>
+      <div>
+        <form >
+          <div className="container">
+            <h1>Sign Up</h1>
+            <p>Please fill in this form to create an account.</p>
+            <hr />
+            <label htmlFor="name">Username</label>
+            <input
+              type="text"
+              placeholder="Username"
+              name="name"
+              required
+            />
+             <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              required
+            />
+            <label htmlFor="psw">Password</label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              name="psw"
+              required
+            />
+             <label htmlFor="psw">Repeat Password</label>
+            <input
+              type="password"
+              placeholder="Repeat Password"
+              name="psw"
+              required
+            />
+            <div className="clearfix">
+              <button type="button" className="cancelbtn">
+                Cancel
+              </button>
+              <button type="submit" className="signupbtn">
+                Sign Up
+              </button>
             </div>
-        </div>
-    )
+          </div>
+        </form>
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
 }
 export default SignUp;
