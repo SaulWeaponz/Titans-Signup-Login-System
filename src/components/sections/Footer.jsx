11a993c6960ce80319fc7ce5f002b59d1233bf86
SaulWeaponz <saulweaponz@gmail.com>
2025-05-28 12:03:22 +0300

    Add changes in footer text

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -2,12 +2,12 @@ function Footer(){
     return(
        <div className="bottom">
        <div className="footer1">
-            <h2>Email:saulweponz@gmail.com</h2>
+            <h2>Titans Fc</h2>
             <h2>Contact: 0752935106</h2>
         </div>
         <div>
             <hr />
-             <h2 className="footer2">&copy;  {new Date().getFullYear()} wpnzfarmAfrica.com</h2>
+             <h2 className="footer2">&copy;  {new Date().getFullYear()} wwwTitans.com</h2>
         </div>
         </div>
     )
