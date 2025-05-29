456ee03ef619714e15bf8ebebf5fff9f6e5dec08
SaulWeaponz <saulweaponz@gmail.com>
2025-05-29 09:04:45 +0300

    Add state, handleChange and handleSubmit logics

--- src/components/pages/SignUp.jsx
+++ src/components/pages/SignUp.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
 import Footer from "../sections/Footer";
 import './pages.css'
 
 function SignUp() {
+  const [formdata, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    repeatpassword: ""
+  })
+  const handleChange =(eventObj, property) => {
+    let value = eventObj.target.value;
+    setFormData({...formdata, [property]: value});
+  }
+   const handleSubmit =(e)=>{
+    e.preventDefault();
+    setFormData({username: "",email:"",password:"",repeatpassword:""})
+    console.log(formdata)
+   }
   return (
     <div>
         <div className="container1">
@@ -18,6 +34,8 @@ function SignUp() {
               type="text"
               placeholder="Username"
               name="name"
+              value={formdata.username}
+              onChange={(e)=>handleChange(e, "username")}
               required
             />
              <label htmlFor="email">Email</label>
@@ -25,6 +43,8 @@ function SignUp() {
               type="email"
               placeholder="Email"
               name="email"
+              value={formdata.email}
+              onChange={(e)=>handleChange(e, "email")}
               required
             />
             <label htmlFor="psw">Password</label>
@@ -32,6 +52,8 @@ function SignUp() {
               type="password"
               placeholder="Enter Password"
               name="psw"
+              value={formdata.password}
+              onChange={(e)=>handleChange(e, "password")}
               required
             />
              <label htmlFor="psw">Repeat Password</label>
@@ -39,15 +61,20 @@ function SignUp() {
               type="password"
               placeholder="Repeat Password"
               name="psw"
+              value={formdata.repeatpassword}
+              onChange={(e)=>handleChange(e, "repeatpassword")}
               required
             />
             <div className="clearfix">
               <button type="button" className="cancelbtn">
                 Cancel
               </button>
-              <button type="submit" className="signupbtn">
+              <button type="submit" className="signupbtn" onClick={handleSubmit}>
                 Sign Up
               </button>
+            </div>
+            <div>
+              <p>Already have an account? Login!</p>
             </div>
           </div>
         </form>
