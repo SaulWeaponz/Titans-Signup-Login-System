b7ced32c1fbcecbee367d0ec60f0d076ca7f240c
SaulWeaponz <saulweaponz@gmail.com>
2025-05-30 23:21:16 +0300

    Add protected Dasboard Route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Home from "./components/pages/Home";
 import Login from "./components/pages/Login";
 import SignUp from "./components/pages/SignUp";
 import NavBar from "./components/sections/Navbar";
+import Dashboard from "./components/pages/Dashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -14,7 +15,16 @@ function App() {
             <Route path="/" element={<Home/>}/>
             <Route path="/SignUp" element={<SignUp/>}/>
             <Route path="/Login" element={<Login/>}/>
-          </Routes>
+            {/* Protected route */}
+          <Route
+           path="/dashboard"
+           element={
+           <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+            </Routes>
       </BrowserRouter>
     </>
   )
