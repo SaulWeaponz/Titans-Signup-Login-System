c871bd906774d12820ef9dd74de6991dc9b60418
SaulWeaponz <saulweaponz@gmail.com>
2025-05-30 23:15:55 +0300

    Add welcome page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,17 +1,28 @@
-import Footer from "../sections/Footer"
+import { Link } from "react-router-dom";
+import "./pages.css";
+import Footer from "../sections/Footer";
 
-function Home(){
-    return(
-        <div>
-            <div></div>
-            <div>
-
-            </div>
-            <div>
-                <Footer/>
-             </div>
+function Welcome() {
+  return (
+<div>
+    <div className="welcome-container">
+      <div className="welcome-box">
+         <h1>TITANS FC</h1>
+        <h1>Welcome to Our Platform </h1>
+        <p>Your journey starts here. Please sign up or log in to continue.</p>
+        <div className="button-group">
+          <Link to="/signup" className="btn btn-primary">Sign Up</Link>
+          <Link to="/login" className="btn btn-secondary">Login</Link>
         </div>
-       
-    )
+      </div>
+      <div>
+      </div>
+    </div>
+    <div>
+        <Footer/>
+    </div>
+    </div>
+  );
 }
-export default Home;
+
+export default Welcome;
